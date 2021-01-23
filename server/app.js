2aac0b3363d19759714e46aa75afd8bf5472f621
Enzomon19 <malstron4@hotmail.es>
2021-01-23 00:46:17 -0500

    CRUD para Student y Subject
    Implementación de los tres modelos y los routes para todas
    las colecciones, la aplicación podrá ir creciendo sobre la marcha,
    así que tengan eso en cuenta.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,16 @@ const port = 3000
 
 const db = require('./db')
 const profesorRouter = require('./routes/Profesor')
+const studentRouter = require('./routes/Student')
+const subjectRouter = require('./routes/Subject')
 
 app.use(express.json())
 
 db.on('error', console.error.bind(console,'connection error: '));
 
 app.use(profesorRouter)
+app.use(studentRouter)
+app.use(subjectRouter)
 
 app.listen(port, () => {
    console.log(`Port ${port} is listening`);
--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose
    .connect('mongodb://localhost:27017/fisidb',{
       useNewUrlParser: true,
-      useUnifiedTopology : true
+      useUnifiedTopology : true,
+      useFindAndModify : false
    })
    .catch( error => {
       console.error(error.message);
